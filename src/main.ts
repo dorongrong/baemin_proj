0f8a46c0ea4388503b2addf81a20de8908bf6c79
dorongrong <84131419+dorongrong@users.noreply.github.com>
2022-05-08 11:31:57 +0900

    main (workflows)

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import BootstrapVue3 from "bootstrap-vue-3";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+createApp(App).use(router).use(BootstrapVue3).mount("#app");
